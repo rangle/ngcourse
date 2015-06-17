40c072df513cf9c921b3fdc912e3dd439bca04c8
Varun Vachhar <varunvachhar@gmail.com>
2015-06-17 13:12:25 -0400

    end of controller section

--- client/app/components/main/main-controller.js
+++ client/app/components/main/main-controller.js
@@ -1,7 +1,14 @@
 'use strict';
 
-angular.module('ngcourse')
+  angular.module('ngcourse')
 
-.controller('MainCtrl', function () {
+  .controller('MainCtrl', function($log) {
+    var vm = this;
+    vm.isAuthenticated = false;
 
-});
+    vm.login = function(username, password) {
+      vm.isAuthenticated = true;
+      vm.username = username;
+      vm.password = password;
+    };
+  });
--- client/app/sections/task-list/task-list-controller.js
+++ client/app/sections/task-list/task-list-controller.js
@@ -2,6 +2,25 @@
 
 angular.module('ngcourse')
 
-.controller('TaskListCtrl', function () {
+.controller('TaskListCtrl', function($log) {
+  var vm = this;
+  vm.numberOfTasks = 0;
+  vm.addTask = function() {
+    vm.numberOfTasks += 1;
+  };
 
+  vm.tasks = [
+    {
+      owner: 'alice',
+      description: 'Build the dog shed.'
+    },
+    {
+      owner: 'bob',
+      description: 'Get the milk.'
+    },
+    {
+      owner: 'alice',
+      description: 'Fix the door handle.'
+    }
+  ];
 });
