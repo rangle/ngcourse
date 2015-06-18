72306a63d5674e3b1bf3055c9e4f773b42281aa1
Varun Vachhar <varunvachhar@gmail.com>
2015-06-18 13:40:49 -0400

    cleanup

--- client/app/sections/examples/ngc-user-directive-with-template-url.js
+++ client/app/sections/examples/ngc-user-directive-with-template-url.js
@@ -1,32 +1,13 @@
 'use strict';
 
 angular.module('ngcourse-example-directives')
-  .controller('NgcUserDirectiveCtrl', function (users, $attrs) {
-    var vm = this;
-    vm.user = users.getUser(vm.username);
 
-    vm.banUser = function() {
-      ...
-      vm.onBan({
-        user: vm.user
-      });
-    }
-  })
-
-  .directive('ngcUser', function () {
-    return {
-      replace: true,
-      restrict: 'E',
-      scope: {
-        username: '=',
-        onBan: '&'
-      },
-      controller: 'NgcUserDirectiveCtrl',
-      controllerAs:'ngcUserCtrl',
-      bindToController: true,
-      template: '<span>{{ user }}</span>'
-    };
-  });
-
-
-<ngc-user username="parent.user" on-ban="parent.handleBan(user)"></ngc-user>
+.directive('ngcUserWithTemplateUrl', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      userDisplayName: '='
+    },
+    templateUrl: 'app/sections/examples/ngc-user-directive-with-template-url.html'
+  };
+});
