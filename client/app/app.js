86182764966789a283c7fcb70c27d61f6837e64c
Sonu Kapoor <sonu.kapoor@rangle.io>
2017-03-08 10:13:16 -0500

    created base empty app for cbc-training

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,1 @@
-'use strict';
-
-angular.module('ngcourse', [
-  'ngcourse.tasks',
-  'ngcourse.router',
-  'ngcourse-example-directives',
-  'ngcourse.users'
-])
-
-.constant('API_BASE_URL', 'http://localhost:3000');
+// create your angular module here
